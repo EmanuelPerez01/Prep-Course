f6e6df05bb69d841b12d96c174460c13879a173c
Emanuel Pérez <emanuelperezlizarraga1@gmail.com>
2022-06-17 00:48:13 -0300

    Terminado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -151,19 +151,16 @@ function operadoresLogicos(num1, num2, num3) {
   if (num1=== 0 || num2===0 || num3===0){
     return "Error"
   }
-  if (num1<0 || num2<0 || num3< 0){
+  else if(num1<0 || num2<0 || num3< 0){
     return "Hay negativos";
   }
-  if(num1 > num2,num3){
+  else if(num3> num1 && num3>num2){
+    return num3+1
+  }
+  else if(num1>num2 && num1>num3 &&num1>0){
     return "Número 1 es mayor y positivo"
   }
-  if (num3 > num1 || num3> num2 && num1 > 0){
-    return num3 + 1
-  }
-   
-  else {
-    return false
-  }
+  return false
 }
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
